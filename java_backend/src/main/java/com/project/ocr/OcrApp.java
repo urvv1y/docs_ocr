184cefa4150ff2d116b95b736589888f6bcd5b15
urvv1y <urvalek.lukas21@gmail.com>
2026-07-10 20:11:39 +0200

    add: java main class funcionality - rest api controll

--- java_backend/src/main/java/com/project/ocr/OcrApp.java
+++ java_backend/src/main/java/com/project/ocr/OcrApp.java
@@ -1,4 +1,10 @@
 package com.project.ocr;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class OcrApp {
+    public static void main(String[] args) {
+        SpringApplication.run(OcrApp.class, args);
+    }
 }
---
+++ java_backend/src/main/java/com/project/ocr/OrcControll.java
@@ -0,0 +1,46 @@
+package com.project.ocr;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author urvv1y
+ * REST controller, creates the endpoint
+ */
+@RestController
+@RequestMapping("/api")
+public class OrcControll {
+    private final RestClient restClient;
+
+    public OrcControll() {
+        this.restClient = RestClient.create("http.//localhost:8000");
+    }
+
+    @PostMapping("/upload/")
+    public ResponseEntity<String> uploadImage(MultipartFile file, String lang) {
+        try {
+            MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+            body.add("file", file.getResource());
+            body.add("lang", lang);
+
+            String response = restClient.post()
+                    .uri("/extract")
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .body(body)
+                    .retrieve()
+                    .body(String.class);
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+}
