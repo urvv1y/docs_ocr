6c84223e1fb2bf9f132dd7fd3ad07caf065b1aa9
urvv1y <urvalek.lukas21@gmail.com>
2026-07-10 17:10:16 +0200

    readibility fix

--- python_ocr/main.py
+++ python_ocr/main.py
@@ -1,4 +1,6 @@
 # imports needed for the project
+from email.mime import image
+
 import pytesseract
 from PIL import Image
 
@@ -7,13 +9,15 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 
 def load_image_czech(img):
 
-    opened_image = Image.open(img)
-    image = pytesseract.image_to_string(opened_image, lang="ces")
-    return image
+    opened_image = Image.open(img).convert("L")
+    image_text = pytesseract.image_to_string(opened_image, lang="ces")
+    return image_text
 
 def load_image_english(img):
-    opened_image = Image.open(img)
-    image = pytesseract.image_to_string(opened_image, lang="eng")
-    return image
+    opened_image = Image.open(img).convert("L")
+    image_text = pytesseract.image_to_string(opened_image, lang="eng")
+    return image_text
+
+print(load_image_czech("doc_test.jpg"))
 
 
