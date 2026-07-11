ad06e5a113430c4336a20ac1e19f25fd8cdf11de
urvv1y <urvalek.lukas21@gmail.com>
2026-07-11 10:52:52 +0200

    add: class receipt

---
+++ java_backend/src/main/java/com/project/ocr/Receipt.java
@@ -0,0 +1,64 @@
+package com.project.ocr;
+
+/**
+ * @author urvvy1
+ * Document: receipt
+ */
+public class Receipt extends Document {
+    private String store;
+    private String date;
+    private String paymentMethod;
+    private String total;
+
+    /**
+     * Constructor for the database
+     */
+    public Receipt() {}
+
+    /**
+     * Constructor
+     * @param store the store
+     * @param date the date of the receipt issuance
+     * @param paymentMethod the payment method - cash or card
+     * @param total the sum of paid money
+     */
+    public Receipt(String store, String date, String paymentMethod, String total) {
+        this.store = store; this.date = date; this.paymentMethod = paymentMethod; this.total = total;
+    }
+
+    // Getters
+
+    public String getStore() {
+        return store;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    // Setter
+
+    public void setStore(String store) {
+        this.store = store;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+}
