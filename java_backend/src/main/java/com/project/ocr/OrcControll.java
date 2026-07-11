77f14bde816dec3d45a1b9bf12d96f032c3d6311
urvv1y <urvalek.lukas21@gmail.com>
2026-07-11 11:13:02 +0200

    feat: refactor

--- java_backend/src/main/java/com/project/ocr/OrcControll.java
+++ java_backend/src/main/java/com/project/ocr/OrcControll.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.multipart.MultipartFile;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 
 /**
  * @author urvv1y
@@ -19,9 +21,13 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api")
 public class OrcControll {
     private final RestClient restClient;
+    private final ReceiptRepository receiptRepository;
+    private final ObjectMapper objectMapper;
 
-    public OrcControll() {
+    public OrcControll(ReceiptRepository receiptRepository) {
         this.restClient = RestClient.create("http://localhost:8000");
+        this.receiptRepository = receiptRepository;
+        this.objectMapper = new ObjectMapper();
     }
 
     @PostMapping("/upload")
@@ -37,8 +43,18 @@ public class OrcControll {
                     .body(body)
                     .retrieve()
                     .body(String.class);
+            JsonNode root = objectMapper.readTree(response);
+            JsonNode dataNode = root.path("data");
 
-            return ResponseEntity.ok(response);
+            String store = dataNode.hasNonNull("Provozovatel") ? dataNode.get("Provozovatel").asString() : dataNode.path("Merchant").asString(null);
+            String date = dataNode.hasNonNull("Datum") ? dataNode.get("Datum").asString() : dataNode.path("Date").asString(null);
+            String payment = dataNode.hasNonNull("Platba")? dataNode.get("Platba").asString() : dataNode.path("Total").asString(null);
+            String total = dataNode.hasNonNull("Celkem") ? dataNode.get("Celkem").asString() : dataNode.path("Total").asString(null);
+
+            Receipt receipt = new Receipt(store, date, payment, total);
+            receiptRepository.save(receipt);
+
+            return ResponseEntity.ok("Document read successfully - doc_id: " + receipt.getId());
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
