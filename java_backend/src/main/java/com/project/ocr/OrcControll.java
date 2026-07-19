1c0718fd0b0a14a5c8c77a54c055093372b224ba
urvv1y <urvalek.lukas21@gmail.com>
2026-07-19 12:34:43 +0200

    controll class invoice integration

--- java_backend/src/main/java/com/project/ocr/OrcControll.java
+++ java_backend/src/main/java/com/project/ocr/OrcControll.java
@@ -1,5 +1,6 @@
 package com.project.ocr;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
@@ -24,11 +25,13 @@ import java.util.List;
 public class OrcControll {
     private final RestClient restClient;
     private final ReceiptRepository receiptRepository;
+    private final InvoiceRepository invoiceRepository;
     private final ObjectMapper objectMapper;
 
-    public OrcControll(ReceiptRepository receiptRepository) {
+    public OrcControll(ReceiptRepository receiptRepository, InvoiceRepository invoiceRepository) {
         this.restClient = RestClient.create("http://localhost:8000");
         this.receiptRepository = receiptRepository;
+        this.invoiceRepository = invoiceRepository;
         this.objectMapper = new ObjectMapper();
     }
 
@@ -62,10 +65,64 @@ public class OrcControll {
         }
 
     }
+
+    @PostMapping("/upload-invoice")
+    public ResponseEntity<String> uploadInvoiceImage(@RequestParam("file") MultipartFile file, @RequestParam(value = "lang", defaultValue = "ces") String lang) {
+        try {
+            MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+            body.add("file", file.getResource());
+            body.add("lang", lang);
+
+            String response = restClient.post()
+                    .uri("/extract")
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .body(body)
+                    .retrieve()
+                    .body(String.class);
+            JsonNode root = objectMapper.readTree(response);
+            JsonNode dataNode = root.path("data");
+
+
+            String seller = dataNode.hasNonNull("Provozovatel") ? dataNode.get("Provozovatel").asString() : dataNode.path("Merchant").asString(null);
+            String invoiceDate = dataNode.hasNonNull("Datum") ? dataNode.get("Datum").asString() : dataNode.path("Date").asString(null);
+            String paymentMethod = dataNode.hasNonNull("Platba") ? dataNode.get("Platba").asString() : dataNode.path("Payment").asString(null);
+            String totalPrice = dataNode.hasNonNull("Celkem") ? dataNode.get("Celkem").asString() : dataNode.path("Total").asString(null);
+
+            String invoiceNumber = dataNode.hasNonNull("Cislo_faktury") ? dataNode.get("Cislo_faktury").asString() : dataNode.path("Invoice_Number").asString(null);
+            String dueDate = dataNode.hasNonNull("Datum_splatnosti") ? dataNode.get("Datum_splatnosti").asString() : dataNode.path("Due_Date").asString(null);
+            String customer = dataNode.hasNonNull("Odberatel") ? dataNode.get("Odberatel").asString() : dataNode.path("Customer").asString(null);
+            String bank = dataNode.hasNonNull("Banka") ? dataNode.get("Banka").asString() : dataNode.path("Bank").asString(null);
+
+            Invoice invoice = new Invoice();
+
+            invoice.setInvoiceNumber(invoiceNumber);
+            invoice.setInvoiceDate(invoiceDate);
+            invoice.setDueDate(dueDate);
+            invoice.setSeller(seller);
+            invoice.setCustomer(customer);
+            invoice.setTotalPrice(totalPrice);
+            invoice.setPaymentMethod(paymentMethod);
+            invoice.setBank(bank);
+
+            invoiceRepository.save(invoice);
+            return ResponseEntity.ok("Invoice saved " + invoice.getId());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+
     @GetMapping("/receipts")
     public ResponseEntity<List<Receipt>> getAllReceipts() {
         List<Receipt> allReceipts = receiptRepository.findAll();
+        allReceipts.stream().sorted();
         return ResponseEntity.ok(allReceipts);
     }
 
+    @GetMapping("/invoices")
+    public ResponseEntity<List<Invoice>> getAllInvoices() {
+        List<Invoice> invoices = invoiceRepository.findAll();
+        invoices.stream().sorted();
+        return ResponseEntity.ok(invoices);
+    }
 }
